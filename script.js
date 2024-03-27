2f901cbc14db847a448ae14e1d92af324d6bc2ad
ALexandru <alexneguleanu@gmail.com>
2024-03-27 11:03:27 +0200

    Finished pick function.

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ const locations = [
       "Go to town square",
       "Go to town square",
     ],
-    "button functions": [goTown, goTown, goTown],
+    "button functions": [goTown, goTown, easterEgg],
     text: 'The monster screams "Arg" as it dies. You gain experience points and find gold.',
   },
   {
@@ -289,6 +289,21 @@ function pickEight() {
 function pick(guess) {
   let numbers = [];
   while (numbers.length < 10) {
-    numbers.push();
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+
+  if (numbers.indexOf(guess) !== -1) {
+    text.innerText += "Right! You win 20 gold!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
   }
 }
